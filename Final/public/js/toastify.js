19a55431107668dc9687862f1b862cc964a0959d
m3mad <m.emadd149@gmail.com>
2025-01-19 19:37:24 +0200

    Update toastify.js

--- Final/public/js/toastify.js
+++ Final/public/js/toastify.js
@@ -1,1 +1,12 @@
+export default (text, color) => {
+  Toastify({
+    text,
+    duration: 3000,
+    gravity: "top",
+    position: "right",
+    stopOnFocus: true,
+    backgroundColor: color,
+    close: true,
+  }).showToast();
+};
 
